7f9f89ca52fe70937e73e510606c3cfa82d3d0f4
Dietrich <jonas.dietrich@wur.nl>
2024-04-22 14:45:38 +0200

    init and version

---
+++ MS2LDA/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) WUR
+# This software may be used and distributed in accordance with the terms of the MS2LDA Community License Agreement.
+from .__version__ import __version__
+
+from .MS2LDA_core import run_lda
+# make it similar to llama3 init https://github.com/meta-llama/llama3/blob/main/llama/__init__.py
