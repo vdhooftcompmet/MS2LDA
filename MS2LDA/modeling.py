7dbecb81c55d242be79f0574b0308aa560b5434b
Dietrich <jonas.dietrich@wur.nl>
2024-05-15 09:52:40 +0200

    seperated create spectrum function

--- MS2LDA/modeling.py
+++ MS2LDA/modeling.py
@@ -89,6 +89,43 @@ def extract_motifs(model, top_n=3):
     return motif_features
 
 
+def create_spectrum(motif_k_features, k, frag_tag="frag@", loss_tag="loss@"):
+
+    # identify slicing start
+    frag_start = len(frag_tag)
+    loss_start = len(loss_tag)
+
+    # extract fragments and losses
+    fragments = [ (float(feature[frag_start:]), float(importance)) for feature, importance in motif_k_features if feature.startswith(frag_tag) ]
+    losses = [ (float(feature[loss_start:]), float(importance)) for feature, importance in motif_k_features if feature.startswith(loss_tag) ]
+
+    # sort features based on mz value
+    sorted_fragments, sorted_fragments_intensities = zip(*sorted(fragments)) if fragments else (np.array([]), np.array([]))
+    sorted_losses, sorted_losses_intensities = zip(*sorted(losses)) if losses else (np.array([]), np.array([]))
+
+    # normalize intensity over fragments and losses
+    intensities = list(sorted_fragments_intensities) + list(sorted_losses_intensities)
+    max_intensity = np.max(intensities)
+    normalized_intensities = np.array(intensities) / max_intensity
+
+    # split fragments and losses
+    normalized_frag_intensities = normalized_intensities[:len(sorted_fragments)]
+    normalized_loss_intensities = normalized_intensities[len(sorted_fragments):]
+
+    # create spectrum object
+    spectrum = Spectrum(
+        mz=np.array(sorted_fragments),
+        intensities=np.array(normalized_frag_intensities),
+        metadata={
+            "id": f"motif_{k}",
+        }
+    )
+    spectrum.losses = Fragments(mz=np.array(sorted_losses), intensities=np.array(normalized_loss_intensities))
+
+    return spectrum
+
+
+
 def create_motif_spectra(motif_features):
     """creates a matchms spectrum object for the found motifs
     
@@ -102,33 +139,7 @@ def create_motif_spectra(motif_features):
     motif_spectra = []
         
     for k, motif_k_features in enumerate(motif_features):
-
-        # extract fragments and losses
-        fragments = [ (float(feature[5:]), float(importance)) for feature, importance in motif_k_features if feature.startswith("frag@") ]
-        losses = [ (float(feature[5:]), float(importance)) for feature, importance in motif_k_features if feature.startswith("loss@") ]
-
-        # sort features based on mz value
-        sorted_fragments, sorted_fragments_intensities = zip(*sorted(fragments)) if fragments else (np.array([]), np.array([]))
-        sorted_losses, sorted_losses_intensities = zip(*sorted(losses)) if losses else (np.array([]), np.array([]))
-
-        # normalize intensity over fragments and losses
-        intensities = list(sorted_fragments_intensities) + list(sorted_losses_intensities)
-        max_intensity = np.max(intensities)
-        normalized_intensities = np.array(intensities) / max_intensity
-   
-        normalized_frag_intensities = normalized_intensities[:len(sorted_fragments)]
-        normalized_loss_intensities = normalized_intensities[len(sorted_fragments):]
-
-        # create motif spectrum
-        motif_spectrum = Spectrum(
-            mz=np.array(sorted_fragments),
-            intensities=np.array(normalized_frag_intensities),
-            metadata={
-                "id": f"motif_{k}",
-            }
-        )
-        motif_spectrum.losses = Fragments(mz=np.array(sorted_losses), intensities=np.array(normalized_loss_intensities))
-
+        motif_spectrum = create_spectrum(motif_k_features, k)
         motif_spectra.append(motif_spectrum)
 
     return motif_spectra
