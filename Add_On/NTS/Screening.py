0e87396d67fc8adf489204b0ac1f745bf3461a3d
Dietrich <jonas.dietrich@wur.nl>
2024-07-23 10:10:44 +0200

    bugfix: intensities and mz switched

--- Add_On/NTS/Screening.py
+++ Add_On/NTS/Screening.py
@@ -1,5 +1,3 @@
-import matchms.filtering as msfilters
-
 from scipy.stats import pearsonr as calc_pearson
 import numpy as np
 import pandas as pd
@@ -22,17 +20,8 @@ def match_spectral_overlap(spectrum_1, spectrum_2, margin_fragments=0.005, margi
     RETURNS:
         overlaping_fragments (list): list for overlaping mz values of fragments
         overlaping_losses (list): list for overlaping mz values of losses
+        pearson_score (float): pearson score for spectra with more than 1 overlaping feature
     """
-
-    spectrum_1_fragments_mz = spectrum_1.peaks.mz
-    spectrum_1_fragments_intensities = spectrum_1.peaks.intensities
-    spectrum_1_losses_mz = spectrum_1.losses.mz
-    spectrum_1_losses_intensities = spectrum_1.losses.intensities
-
-    spectrum_2_fragments_mz = spectrum_2.peaks.mz
-    spectrum_2_fragments_intensities = spectrum_2.peaks.intensities
-    spectrum_2_losses_mz = spectrum_2.losses.mz
-    spectrum_2_losses_intensities = spectrum_2.losses.intensities
 
 
     def find_overlap(arr1, arr2, margin):
@@ -45,6 +34,8 @@ def match_spectral_overlap(spectrum_1, spectrum_2, margin_fragments=0.005, margi
         
         RETURNS:
             overlap (list): overlaping float values within the error margin
+            idx_spectrum_1_intensities (list): indices of overlaping features in spectrum 1
+            idx_spectrum_2_intensities (list): indices of overlaping features in spectrum 2
         """
         i, j = 0, 0
         overlap = []
@@ -62,11 +53,26 @@ def match_spectral_overlap(spectrum_1, spectrum_2, margin_fragments=0.005, margi
 
         return overlap, idx_spectrum_1_intensities, idx_spectrum_2_intensities
     
-    overlaping_fragments, idx_fragments_1_intensities, idx_fragments_2_intensities = find_overlap(spectrum_1_fragments_mz, spectrum_2_fragments_mz, margin_fragments)
-    overlaping_losses, idx_losses_1_intensities, idx_losses_2_intensities = find_overlap(spectrum_1_losses_mz, spectrum_2_losses_intensities, margin_losses)
+    # spectrum 1 fragments and losses
+    spectrum_1_fragments_mz = spectrum_1.peaks.mz
+    spectrum_1_fragments_intensities = spectrum_1.peaks.intensities
+    spectrum_1_losses_mz = spectrum_1.losses.mz
+    spectrum_1_losses_intensities = spectrum_1.losses.intensities
 
+    # spectrum 2 fragments and losses
+    spectrum_2_fragments_mz = spectrum_2.peaks.mz
+    spectrum_2_fragments_intensities = spectrum_2.peaks.intensities
+    spectrum_2_losses_mz = spectrum_2.losses.mz
+    spectrum_2_losses_intensities = spectrum_2.losses.intensities
+    
+    # find overlaping features
+    overlaping_fragments, idx_fragments_1_intensities, idx_fragments_2_intensities = find_overlap(spectrum_1_fragments_mz, spectrum_2_fragments_mz, margin_fragments)
+    overlaping_losses, idx_losses_1_intensities, idx_losses_2_intensities = find_overlap(spectrum_1_losses_mz, spectrum_2_losses_mz, margin_losses)
+
+    # pearson score for intensity trend
     intensities_spectrum_1 = list(spectrum_1_fragments_intensities[idx_fragments_1_intensities]) + list(spectrum_1_losses_intensities[idx_losses_1_intensities])
     intensities_spectrum_2 = list(spectrum_2_fragments_intensities[idx_fragments_2_intensities]) + list(spectrum_2_losses_intensities[idx_losses_2_intensities])
+
     if len(intensities_spectrum_1) >= 2:
         pearson_score = calc_pearson(intensities_spectrum_1, intensities_spectrum_2)[0]
     else:
@@ -76,7 +82,7 @@ def match_spectral_overlap(spectrum_1, spectrum_2, margin_fragments=0.005, margi
 
 
 
-def calc_overlaping_stats(motif, spectrum, overlaping_fragments, overlaping_losses):
+def calc_overlaping_stats(motif, overlaping_fragments, overlaping_losses):
     """calculates the number of overlaping features and their cumulative intensity
     
     ARGS:
@@ -88,29 +94,22 @@ def calc_overlaping_stats(motif, spectrum, overlaping_fragments, overlaping_loss
         n_overlaping_features (int): number of features that did overlap between motif and query spectrum
         sum_overlaping_features_intensities (float): consensus intensity for all features that overlap between motif and query spectrum
     """
+
+    # motif intensities of overlaping fragments
     motif_overlaping_fragments_intensities = []
-    spectrum_overlaping_fragments_intensities = []
     for overlaping_fragment in overlaping_fragments:
         motif_fragment_index = np.where(motif.peaks.mz == overlaping_fragment)[0][0]
         motif_fragment_intensity = motif.peaks.intensities[motif_fragment_index]
         motif_overlaping_fragments_intensities.append(motif_fragment_intensity)
 
-        #spectrum_fragment_index = np.where(spectrum.peaks.mz == overlaping_fragment)[0][0]
-        #spectrum_fragment_intensity = spectrum.peaks.intensities[spectrum_fragment_index]
-        #spectrum_overlaping_fragments_intensities.append(spectrum_fragment_intensity)
-
+    # motif intensities of overlaping losses
     motif_overlaping_losses_intensities = []
-    spectrum_overlaping_losses_intensities = []
     for overlaping_loss in overlaping_losses:
         motif_loss_index = np.where(motif.losses.mz == overlaping_loss)[0][0]
         motif_loss_intensity = motif.losses.intensities[motif_loss_index]
         motif_overlaping_losses_intensities.append(motif_loss_intensity)
 
-        #spectrum_loss_index = np.where(spectrum.losses.mz == overlaping_loss)[0][0]
-        #spectrum_loss_intensity = spectrum.losses.intensities[spectrum_loss_index]
-        #spectrum_overlaping_losses_intensities.append(spectrum_loss_intensity)
-
-
+    # number of overlaping features, and cumulative intensity of overlaping features
     motif_overlaping_features_intensities = motif_overlaping_fragments_intensities + motif_overlaping_losses_intensities
     motif_sum_overlaping_features_intensities= np.sum(motif_overlaping_features_intensities)
     n_overlaping_features = len(motif_overlaping_features_intensities)
@@ -127,16 +126,16 @@ def screen_score(n_overlaping_features, sum_overlaping_features_intensities, pea
         sum_overlaping_features_intensities (float): consensus intensity for all features that overlap between motif and query spectrum
         
     RETURNS:
-        (character): A,B,C or D if it fits one of the categories else None
+        (character): A, B, C or D if it fits one of the categories else None
     """
 
-    if n_overlaping_features >= 3 and sum_overlaping_features_intensities >= 0.9 and pearson_score > 0.8:
+    if n_overlaping_features >= 3 and sum_overlaping_features_intensities >= 0.9 and pearson_score >= 0.8:
         return "A"
-    elif n_overlaping_features == 1 and sum_overlaping_features_intensities >= 0.9:
+    elif n_overlaping_features == 2 and sum_overlaping_features_intensities >= 0.9 and pearson_score >= 0.8:
         return "B"
-    elif n_overlaping_features >= 2 and sum_overlaping_features_intensities >= 0.7:
+    elif n_overlaping_features >= 2 and sum_overlaping_features_intensities >= 0.5 and pearson_score >= 0.6:
         return "C"
-    elif n_overlaping_features >= 2 and sum_overlaping_features_intensities >= 0.5:
+    elif n_overlaping_features >= 1 and sum_overlaping_features_intensities >= 0.9:
         return "D"
     else:
         return None
@@ -157,8 +156,9 @@ def run_screen(motif, spectra):
     A,B,C,D = [],[],[],[]
     for spectrum in spectra:
         overlaping_fragments, overlaping_losses, pearson_score = match_spectral_overlap(motif, spectrum)
-        n_overlaping_features, sum_overlaping_features_intensities = calc_overlaping_stats(motif, spectrum, overlaping_fragments, overlaping_losses)
+        n_overlaping_features, sum_overlaping_features_intensities = calc_overlaping_stats(motif, overlaping_fragments, overlaping_losses)
         match_category = screen_score(n_overlaping_features, sum_overlaping_features_intensities, pearson_score)
+
         if match_category == "A":
             A.append(spectrum)
         elif match_category == "B":
@@ -216,6 +216,7 @@ def save_as_csv(spectra, smiles):
 
 
 if __name__ == "__main__":
-    run_screen() # not working
+    pass
+    #run_screen()
 
         
